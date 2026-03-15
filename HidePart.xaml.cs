79b3ac6cab841235f552a8ed0cf4ffbe082e274d
progzone122 <dev@diablosat.cc>
2026-03-15 19:44:18 +0200

    Remove unused StringBuilder code in HidePart.xaml.cs

--- HidePart.xaml.cs
+++ HidePart.xaml.cs
@@ -65,41 +65,33 @@ namespace MakuTweakerNew
 
         private void ContentDialog_CloseButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
         {
-            decimal i = 0;    
+            decimal i = 0;
 
-            if (a.IsChecked == true) i += 1;            
-            if (d.IsChecked == true) i += 8;        
-            if (e.IsChecked == true) i += 16;       
-            if (f.IsChecked == true) i += 32;       
-            if (g.IsChecked == true) i += 64;       
-            if (h.IsChecked == true) i += 128;      
+            if (a.IsChecked == true) i += 1;
+            if (d.IsChecked == true) i += 8;
+            if (e.IsChecked == true) i += 16;
+            if (f.IsChecked == true) i += 32;
+            if (g.IsChecked == true) i += 64;
+            if (h.IsChecked == true) i += 128;
             if (this.i.IsChecked == true) i += 256;
-            if (j.IsChecked == true) i += 512;      
-            if (k.IsChecked == true) i += 1024;     
-            if (l.IsChecked == true) i += 2048;     
-            if (m.IsChecked == true) i += 4096;     
-            if (n.IsChecked == true) i += 8192;     
-            if (o.IsChecked == true) i += 16384;    
-            if (p.IsChecked == true) i += 32768;    
-            if (q.IsChecked == true) i += 65536;    
-            if (r.IsChecked == true) i += 131072;   
-            if (s.IsChecked == true) i += 262144;   
-            if (t.IsChecked == true) i += 524288;   
-            if (u.IsChecked == true) i += 1048576;  
-            if (v.IsChecked == true) i += 2097152;  
-            if (w.IsChecked == true) i += 4194304;  
-            if (x.IsChecked == true) i += 8388608;  
-            if (y.IsChecked == true) i += 16777216;  
-            if (z.IsChecked == true) i += 33554432;  
+            if (j.IsChecked == true) i += 512;
+            if (k.IsChecked == true) i += 1024;
+            if (l.IsChecked == true) i += 2048;
+            if (m.IsChecked == true) i += 4096;
+            if (n.IsChecked == true) i += 8192;
+            if (o.IsChecked == true) i += 16384;
+            if (p.IsChecked == true) i += 32768;
+            if (q.IsChecked == true) i += 65536;
+            if (r.IsChecked == true) i += 131072;
+            if (s.IsChecked == true) i += 262144;
+            if (t.IsChecked == true) i += 524288;
+            if (u.IsChecked == true) i += 1048576;
+            if (v.IsChecked == true) i += 2097152;
+            if (w.IsChecked == true) i += 4194304;
+            if (x.IsChecked == true) i += 8388608;
+            if (y.IsChecked == true) i += 16777216;
+            if (z.IsChecked == true) i += 33554432;
 
-            StringBuilder drives = new StringBuilder();
-            foreach (var child in checkboxpanel.Children)
-            {
-                if (child is CheckBox checkBox && checkBox.IsChecked == true)
-                {
-                    drives.Append(checkBox.Content.ToString());
-                }
-            }
             CloseDialog(i);
         }
     }
